6cc969efe17c2f91d1c9dc6c8ae5f4a314efd684
clgong <101086582+clgong@users.noreply.github.com>
2023-03-26 14:41:34 -0700

    Check if data dir works
    Should be reading data from './data/cleaned_v2/cleaned_listing_and_review_with_polarity.zip'

--- mainapp.py
+++ mainapp.py
@@ -7,59 +7,27 @@ Here's our first attempt at using data to create a table:
 """
 
 import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore',category=DeprecationWarning)
+
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.max_rows', 100)
+
+RANDOM_STATE= 42
+
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 st.title('AirBnb Rentals in Seattle')
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-# df
+# Prepare data
 
-
-st.table(df)
-
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
-
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
-
-
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
-
-
-
-st.text_input("Your name", key="name")
-
-# You can access the value at any point with:
-st.session_state.name
-
-
-
-
-
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
-
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
+# load dataset that has already been cleaned
+raw_df = pd.read_pickle(basepath+'./data/cleaned_v2/cleaned_listing_and_review_with_polarity.zip')
+st.write(raw_df.shape)
