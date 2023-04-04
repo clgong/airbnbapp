d0464a5e1b22b73ad49f6b48e494f479535160ca
AbbyZhai <50309878+AbbyZhai@users.noreply.github.com>
2023-04-03 18:28:59 -0700

    add averaged_perceptron_tagger

--- pages/text_based_recommender.py
+++ pages/text_based_recommender.py
@@ -59,7 +59,7 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-# nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger')
 
 from nltk import word_tokenize, pos_tag
 from nltk.tokenize import RegexpTokenizer
