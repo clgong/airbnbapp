8d0adf7421cd34a47fbc964264807ec7cf3dca61
AbbyZhai <50309878+AbbyZhai@users.noreply.github.com>
2023-04-06 22:04:28 -0700

    uncommented vader_lexicon

--- pages/text_based_recommender.py
+++ pages/text_based_recommender.py
@@ -38,7 +38,7 @@ nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('averaged_perceptron_tagger')
-# nltk.download('vader_lexicon') 
+nltk.download('vader_lexicon') 
 
 from nltk import word_tokenize, pos_tag
 from nltk.tokenize import RegexpTokenizer
